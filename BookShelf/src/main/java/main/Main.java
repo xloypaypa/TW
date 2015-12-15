df90e9f945601a41a97a45fbbfecf7de48e1a3c9
xloypaypa <245532675@qq.com>
2015-12-15 23:28:45 +0800

    add hello. but can't load controller

---
+++ BookShelf/src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xlo on 15/12/15.
+ * it's the main class
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
